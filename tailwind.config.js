c3940a2ab2ace5fd43272b2a331720d1ff368847
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-07-07 13:15:39 -0400

    create html markup for most complex design, which is desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,25 @@ module.exports = {
       xl: '1440px',
     },
     extend: {
-      // colors: {},
+      backgroundImage: {
+        'stats-image-mobile':
+          "url('/develop/assets/images/image-header-mobile.jpg')",
+        'stats-image-desktop':
+          "url('/develop/assets/images/image-header-desktop.jpg')",
+      },
+      colors: {
+        veryDarkBlue: 'hsl(233, 47%, 7%)',
+        darkDesaturatedBlue: 'hsl(244, 38%, 16%)',
+        softViolet: 'hsl(277, 64%, 61%)',
+
+        neutral: 'hsl(0, 0%, 100%)',
+        mainParaWhite: 'hsla(0, 0%, 100%, 0.75)',
+        statsWhite: 'hsla(0, 0%, 100%, 0.6)',
+      },
+      fontFamily: {
+        inter: ['Inter', '"sans-serif"'],
+        deca: ['"Lexend Deca"', '"sans-serif"'],
+      },
     },
   },
   plugins: [],
